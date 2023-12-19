ace05273de809004e459e93452faebe3f891f02d
Krystian Duma <git@krystian.duma.sh>
2023-12-19 21:28:19 +0100

    Example failing test

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use InteractsWithMedia;
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('preview')
+            ->fit(Fit::Crop, 300, 300)
+            ->format('png')
+            ->nonQueued();
+    }
 
     /**
      * The attributes that are mass assignable.
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,26 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $user = \App\Models\User::factory()
+            ->create();
 
-        $response->assertOk();
+        $user
+            ->addMedia(resource_path('dummy.pdf'))
+            ->preservingOriginal()
+            ->toMediaCollection();
+
+        $this->assertNotNull($user->getFirstMediaUrl(conversionName: 'preview'));
     }
 }
